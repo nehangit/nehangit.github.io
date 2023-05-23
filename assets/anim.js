7334d58e3e643b948cf0e9bf7107b9118d2887ec
Nehan Tarefder <nehantarefder@outlook.com>
2023-05-22 23:48:50 -0600

    1 hour cookie timeout

--- assets/anim.js
+++ assets/anim.js
@@ -1,13 +1,12 @@
 const screen = document.querySelector('.scrn');
 
-var dd = new Date();
-var expireTime = dd.getTime() + 7200000;
-dd.setTime(expireTime);
-
 if (document.cookie.indexOf("visited") >= 0) {
     screen.remove();
 } else {
-    document.cookie = "visited=true;expires="+dd.toUTCString()+';';
+    var dd = new Date();
+    var expireTime = dd.getTime() + 3600000;
+    dd.setTime(expireTime);
+    document.cookie = "visited=true; expires=" + dd.toUTCString() +"; path=/;";
     document.addEventListener('DOMContentLoaded', (e) => {
         setTimeout(() => {
             screen.classList.add('fadeout');
