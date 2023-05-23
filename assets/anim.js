832b07b225387ceb2c1bfe238e6e837637430c1f
Nehan Tarefder <nehantarefder@outlook.com>
2023-05-22 21:28:14 -0600

    set cookie expiration

--- assets/anim.js
+++ assets/anim.js
@@ -1,9 +1,13 @@
 const screen = document.querySelector('.scrn');
 
+var dd = new Date();
+var expireTime = dd.getTime() + 7200000;
+dd.setTime(expireTime);
+
 if (document.cookie.indexOf("visited") >= 0) {
     screen.remove();
 } else {
-    document.cookie = "visited";
+    document.cookie = "visited=true;expires="+dd.toUTCString()+';';
     document.addEventListener('DOMContentLoaded', (e) => {
         setTimeout(() => {
             screen.classList.add('fadeout');
